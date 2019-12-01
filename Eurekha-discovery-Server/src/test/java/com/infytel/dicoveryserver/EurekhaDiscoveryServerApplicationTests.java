a87506e4ce994b242f6eba134ebfe29ff92f5b90
PoojaSagar <psnagadiya@gmail.com>
2019-12-01 14:24:22 +0530

    fsgsd

---
+++ Eurekha-discovery-Server/src/test/java/com/infytel/dicoveryserver/EurekhaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infytel.dicoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekhaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
